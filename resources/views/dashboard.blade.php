a52b38f0e1ac5b05c2e653cd06de906985dd5184
Joran Daniël <124999321+JoranDaniel@users.noreply.github.com>
2023-12-13 16:20:17 +0100

    end of day commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,93 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <style>
+        body {
+            font-family: 'Jockey One', sans-serif; // Modified this line
+            font-size: 30px;
+            font-weight: bold;
+            margin: 0;
+            padding: 0;
+            background-image: url('https://cdn.discordapp.com/attachments/1098603596486029313/1182682992594915469/image_1_1.png?ex=6585965e&is=6573215e&hm=f7264f44b46fcf8473b598e1310f951009f2e07e6ca286ff278de31b5605b8b2&');
+            background-repeat: no-repeat;
+            background-size: cover;
+            color: #333;
+        }
+
+        .dashboard-container {
+            background-color: #fff;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+
+        .dashboard-header {
+            background-color: #3498db;
+            color: black;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .dashboard-content {
+            padding: 20px;
+        }
+
+        .navigation-bar {
+            background-color: rgba(255, 255, 255, 0.558);
+            color: black;
+            padding: 10px;
+            text-align: center;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%; // Added this line
+        }
+
+        .navigation-bar a {
+            color: black;
+            text-decoration: none;
+            margin: 0 10px;
+        }
+
+        .user-profile {
+            display: flex;
+            align-items: center;
+            margin-right: 10px;
+        }
+
+        .user-profile img {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+    <div class="dashboard-container">
+        <div class="navigation-bar">
+            <div class="left-navigation">
+                <a href="#">Home</a>
+                <a href="#">Leaderboard</a>
+                <a href="#">Merch</a>
+            </div>
+            <div class="user-profile">
+                <span>{{ __("") }} {{ auth()->user()->name }}</span>
+                <img src="https://media.discordapp.net/attachments/918918944809713715/1162323141595504660/POV_de_wekker_gaat_en_moet_naar_school.gif?ex=653b84c7&is=65290fc7&hm=ca92a5e86a23c39d9b6b74a89755e56412739bfcf5574d781c3e775f72725c4d&" alt="Profile Photo">
             </div>
         </div>
+        </div>
     </div>
-</x-app-layout>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,61 +8,83 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Jockey+One&display=swap" rel="stylesheet">
 
+    <!-- Styles -->
     <style>
-        *{
+        body {
+            font-family: 'Jockey One', sans-serif;
+            font-size: 20px;
+            font-weight: bold;
             margin: 0;
             padding: 0;
-            box-sizing: border-box;
-        }
-        body {
-            font-family: 'Jockey One', sans-serif; /* Updated font-family */
-            background-image: url('https://cdn.discordapp.com/attachments/1098603596486029313/1182682992594915469/image_1_1.png?ex=6585965e&is=6573215e&hm=f7264f44b46fcf8473b598e1310f951009f2e07e6ca286ff278de31b5605b8b2&'); /* Update the path to the image file */
+            background-image: url('https://cdn.discordapp.com/attachments/1098603596486029313/1182682992594915469/image_1_1.png?ex=6585965e&is=6573215e&hm=f7264f44b46fcf8473b598e1310f951009f2e07e6ca286ff278de31b5605b8b2&');
             background-repeat: no-repeat;
             background-size: cover;
-            font-size: 30px;
+            color: #333;
         }
-                .navigation {
-                    background-color: rgba(255, 255, 255, 0.558);
-                    display: flex;
-                    justify-content: flex-start; /* Updated justify-content property */
-                    align-items: center;
-                    padding: 20px;
-                    border-bottom: 1px solid #ccc;
-                    height: 100px;
-                }
-                .navigation a {
-                    color: #333;
-                    text-decoration: none;
-                    margin: 0 10px;
-                    font-weight: bold;
-                }
-                .navigation a:hover {
-                    color: #ff0000;
-                }
-                .content {
-                    max-width: 1200px;
-                    margin: 0 auto;
-                    padding: 40px;
-                    background-color: #ffffff;
-                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                    width: 100%;
-                    position: absolute;
-                    left: 50%;
-                    transform: translateX(-50%);
-                }
-            </style>
-            
-        </head>
-        <body>
-            <div class="navigation">
-                <livewire:welcome.navigation class="navigation" style="background-color: white; justify-content: flex-start;"> <!-- Updated justify-content property -->
-            </div>
 
-            
-                
+        .dashboard-container {
+            background-color: #fff;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+
+        .dashboard-header {
+            background-color: #3498db;
+            color: black;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .dashboard-content {
+            padding: 20px;
+        }
+
+        .navigation-bar {
+            background-color: rgba(255, 255, 255, 0.558);
+            color: black;
+            padding: 10px;
+            text-align: center;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width: 100%;
+        }
+
+        .navigation-bar a {
+            color: black;
+            text-decoration: none;
+            margin: 0 10px;
+        }
+
+        .user-profile {
+            display: flex;
+            align-items: center;
+            margin-right: 10px;
+        }
+
+        .user-profile img {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+    </style>
+
+</head>
+<body class="antialiased">
+    <div class="dashboard-container">
+        <div class="navigation-bar">
+            <div class="left-navigation">
+                <a href="#">Home</a>
+                <a href="#">Leaderboard</a>
+                <a href="#">Merch</a>
             </div>
-           
-        </body>
-        </html>
+            <div class="user-profile">
+                <span>{{ __("") }} {{ auth()->user()->name }}</span>
+                <img src="https://media.discordapp.net/attachments/918918944809713715/1162323141595504660/POV_de_wekker_gaat_en_moet_naar_school.gif?ex=653b84c7&is=65290fc7&hm=ca92a5e86a23c39d9b6b74a89755e56412739bfcf5574d781c3e775f72725c4d&" alt="Profile Photo">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
